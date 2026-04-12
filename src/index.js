7e3604523a0c5bf691d26283964ba20864208037
DevrajSharma988 <devrajsharma6888@gmail.com>
2026-04-12 12:38:03 +0530

    utils and config of express

--- src/index.js
+++ src/index.js
@@ -5,7 +5,23 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path:'./env'
 })
+
+
 connectDB()
+.then(()=>{
+    //if app is not able to listen then use it taaki safe rhe 
+    app.on("error",(error)=>{
+        console.log("error :",error);
+        throw error; 
+    })
+    //yadi subkuch shi hai to listen krado
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running at ${process.env.PORT}`);  
+    })
+})
+.catch((error)=>{
+    console.log("mongo db connection failed :",error);
+})
 
 
 
